1723c7a62250c4ba04c435539807f5bd065caec9
Priyanshu Singh <80763916+priyanXshu@users.noreply.github.com>
2024-03-03 20:52:56 +0530

    QueTube - A youtube clone

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "4673ff5616msh2f8a0ffaed6cf8fp19496ajsne0c05e720653",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
